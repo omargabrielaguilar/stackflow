6219eb74aa6ceea1cfa37b70b13fc0203ac99d20
Omar <saucyfunnel@gmail.com>
2025-02-07 12:02:04 -0500

    lang

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="flex items-center justify-between px-4 py-3 bg-white border-t border-gray-200 sm:px-6">
+    <div class="flex items-center justify-between px-4 py-3 border-t border-gray-200 sm:px-6">
         <div class="flex justify-between flex-1 sm:hidden">
-            <a href="#"
-                class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50">Previous</a>
-            <a href="#"
-                class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50">Next</a>
+            <Link :href="previousUrl"
+                class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50">
+            Anterior</Link>
+            <Link :href="nextUrl"
+                class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50">
+            Despues</Link>
         </div>
         <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
             <div>
@@ -25,7 +27,7 @@
                 </p>
             </div>
             <div>
-                <nav class="inline-flex -space-x-px rounded-md shadow-xs isolate" aria-label="Pagination">
+                <nav class="inline-flex -space-x-px bg-white rounded-md shadow-xs isolate" aria-label="Pagination">
                     <Link v-for="link in meta.links" :href="link.url"
                         class="relative inline-flex items-center px-2 py-2 " :class="{
                             'z-10 bg-indigo-600 text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600': link.active,
@@ -41,7 +43,11 @@
 <script setup>
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/20/solid'
 import { Link } from '@inertiajs/vue3';
-defineProps(['meta']);
+import { computed } from 'vue';
 
+const props = defineProps(['meta']);
+
+const previousUrl = computed(() => props.meta.links[0].url);
+const nextUrl = computed(() => [...props.meta.links].reverse()[0].url);
 
 </script>
