7e4b2b7bb484f7b392ac5230b9587f9318b91b0d
Omar <saucyfunnel@gmail.com>
2025-02-08 20:38:04 -0500

    provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,7 @@
 <?php
 
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\FortifyServiceProvider::class,
-    App\Providers\JetstreamServiceProvider::class,
-];
+// return [
+//     App\Providers\AppServiceProvider::class,
+//     App\Providers\FortifyServiceProvider::class,
+//     App\Providers\JetstreamServiceProvider::class,
+// ];
